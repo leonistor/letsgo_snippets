0e3d1e0728594afd5103f7014f42ace43a2e1f35
Leo Nistor <leo.nistor@gmail.com>
2020-12-27 19:59:06 +0200

    2.7 project structure

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	// change behaviour, match exact, see below
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -27,23 +25,9 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost) // call before Write or WriteHeader
+		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", 405)
 		return
 	}
 	w.Write([]byte("Create new snippet"))
-}
-
-func main() {
-	// make your own, don't use DefaultServeMux with Handle() or HandleFunc()
-	mux := http.NewServeMux()
-	// subtree path, match when starts with pattern, ends with "/"
-	mux.HandleFunc("/", home)
-	// fixed path: exact match, does not end with "/"
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Starting server at :8080")
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
 }
