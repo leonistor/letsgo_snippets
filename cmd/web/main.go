ed6f1d81866c5a56b7681aa725129167955e7f30
Leo Nistor <leo.nistor@gmail.com>
2020-12-28 18:35:59 +0200

    3.1 addr flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":8080", "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -14,7 +18,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server at :8080")
-	err := http.ListenAndServe(":8080", mux)
+	log.Printf("Starting server at %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
