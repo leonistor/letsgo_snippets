f05df4f383770db96f33c6da76d90693f688b0cc
Leo Nistor <leo.nistor@gmail.com>
2020-12-29 23:23:28 +0200

    11.2 users table

--- db/create.sql
+++ db/create.sql
@@ -11,3 +11,21 @@ CREATE INDEX IF NOT EXISTS idx_snippets_created ON snippets(created);
 GRANT ALL ON snippets TO snippetbox;
 
 GRANT ALL ON snippets_id_seq TO snippetbox;
+
+CREATE TABLE IF NOT EXISTS users (
+  id serial PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  hashed_password char(60) NOT NULL,
+  created timestamptz NOT NULL DEFAULT NOW(),
+  active boolean NOT NULL DEFAULT TRUE
+);
+
+ALTER TABLE
+  users
+ADD
+  CONSTRAINT users_uc_email UNIQUE(email);
+
+GRANT ALL ON users TO snippetbox;
+
+GRANT ALL ON users_id_seq TO snippetbox;
