1f6cb5920e3dcd9a577f23d287155bcc0e0a216e
Leo Nistor <leo.nistor@gmail.com>
2020-12-29 15:09:28 +0200

    8.3 form validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"snippetbox/pkg/models"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -52,6 +54,30 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	title := r.PostForm.Get("title")
 	content := r.PostForm.Get("content")
 	expires := r.PostForm.Get("expires")
+
+	errors := make(map[string]string)
+
+	if strings.TrimSpace(title) == "" {
+		errors["title"] = "Title cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		errors["title"] = "Title too long (max 100 characters)"
+	}
+	if strings.TrimSpace(content) == "" {
+		errors["content"] = "Content cannot be blank"
+	}
+	if strings.TrimSpace(expires) == "" {
+		errors["title"] = "Expires cannot be blank"
+	} else if expires != "365" && expires != "7" && expires != "1" {
+		errors["expires"] = "Invalid expire"
+	}
+
+	if len(errors) > 0 {
+		app.render(w, r, "create.page.tmpl", &templateData{
+			FormData:   r.PostForm,
+			FormErrors: errors,
+		})
+		return
+	}
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
