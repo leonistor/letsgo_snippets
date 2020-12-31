2941ef32b8c1c353fa7621c9d352c8c032d40387
Leo Nistor <leo.nistor@gmail.com>
2020-12-31 08:55:18 +0200

    13.3 testutils

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,36 +1,22 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		errorLog: log.New(ioutil.Discard, "", 0),
-		infoLog:  log.New(ioutil.Discard, "", 0),
-	}
+	app := newTestApplication(t)
 
-	ts := httptest.NewTLSServer(app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
+	code, _, body := ts.get(t, "/ping")
+
+	if code != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, code)
 	}
 
-	if rs.StatusCode != http.StatusOK {
-		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
-	}
-
-	defer rs.Body.Close()
-	body, err := ioutil.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
 	if string(body) != "OK" {
 		t.Errorf("want %s; got %s", "OK", string(body))
 	}
